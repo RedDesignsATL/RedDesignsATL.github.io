8668e92702801062a7af37d3b2ebd909465dd4bf
RedDesignsATL <44686162+RedDesignsATL@users.noreply.github.com>
2019-01-21 12:00:04 -0500

    attempt 1

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// // Activate Carousel
+// $("#myCarousel").carousel();
+
+// // Enable Carousel Indicators
+// $(".item").click(function(){
+//   $("#myCarousel").carousel(1);
+// });
+
+// // Enable Carousel Controls
+// $(".carousel-control-prev").click(function(){
+//   $("#myCarousel").carousel("prev");
+// });
+
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//   }
